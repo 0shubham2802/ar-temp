d735e16b65eff12ec545d28b0272bcdab7275a6e
0shubham2802 <shubhamalappant2802@gmail.com>
2025-05-26 01:21:26 +0530

    Fix: ensure required AR assets are present and update asset loading logic

--- app/src/main/java/com/google/ar/core/codelabs/hellogeospatial/HelloGeoRenderer.kt
+++ app/src/main/java/com/google/ar/core/codelabs/hellogeospatial/HelloGeoRenderer.kt
@@ -147,7 +147,7 @@ class HelloGeoRenderer(val context: Context) :
       virtualObjectTexture =
         Texture.createFromAsset(
           render,
-          "models/spatial_marker_baked.png",
+          "../../assets/models/spatial_marker_baked.png",
           Texture.WrapMode.CLAMP_TO_EDGE,
           Texture.ColorFormat.SRGB
         )
@@ -165,7 +165,7 @@ class HelloGeoRenderer(val context: Context) :
       try {
         arrowTexture = Texture.createFromAsset(
           render,
-          "models/materials.mtl",
+          "../../assets/models/materials.mtl",
           Texture.WrapMode.CLAMP_TO_EDGE,
           Texture.ColorFormat.SRGB
         )
@@ -189,7 +189,7 @@ class HelloGeoRenderer(val context: Context) :
       try {
         destinationTexture = Texture.createFromAsset(
           render,
-          "models/materials.mtl",
+          "../../assets/models/materials.mtl",
           Texture.WrapMode.CLAMP_TO_EDGE,
           Texture.ColorFormat.SRGB
         )
